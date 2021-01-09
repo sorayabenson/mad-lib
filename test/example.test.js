6df34dc2119172676b4bb5da3bbd1d1e3bb4d7f7
Parker Sequeira <phsequeira@yahoo.com>
2021-01-08 17:40:30 -0800

    made test and added imputs and button

--- test/example.test.js
+++ test/example.test.js
@@ -3,14 +3,13 @@
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('it should return hi when provided hi', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
-    
+    const expected = 'hi';   
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = noun1'hi';
 
     //Expect
     // Make assertions about what is expected versus the actual result
---
+++ utils.js
@@ -0,0 +1,6 @@
+export function nounFunction(noun1) {
+    return noun1.textContent;
+}
+export function nounFunction(noun1) {
+    return noun1.textContent;
+}
