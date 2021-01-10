f9e4e6e6e7297b2ff96bc53a6cefc8fc316c547b
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-01-09 17:20:42 -0800

    add event listner that replaces fields with poem, connect input boxes to even listener.

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // import functions and grab DOM elements
+/* dont think we need any of these 
+
 const noun1 = document.getElementById('noun1');
 const noun2 = document.getElementById('noun2');
 const noun3 = document.getElementById('noun3');
@@ -10,16 +12,32 @@ const adjective3 = document.getElementById('adjective3');
 const verb1 = document.getElementById('verb1');
 const verb2 = document.getElementById('verb2');
 const verb3 = document.getElementById('verb3');
-const MadLipz = document.getElementById('MadLipz');
+const poem = document.getElementById('poem');
+*/
 
+const madLipz = document.getElementById('MadLipz');
+const inputOne = document.getElementById('nounInputOne');
+const inputTwo = document.getElementById('nounInputTwo');
+const inputThree = document.getElementById('adjectiveInputOne');
+const inputFour = document.getElementById('adjectiveInputTwo');
+const inputFive = document.getElementById('verbInputOne');
+const inputSix = document.getElementById('adjectiveInputThree');
+const inputSeven = document.getElementById('nounInputThree');
+const inputEight = document.getElementById('nounInputFour');
+const inputNine = document.getElementById('verbInputTwo');
+const inputTen = document.getElementById('nounInputFive');
+const inputEleven = document.getElementById('verbInputThree');
 
+const inputWrapper = document.getElementById('inputWrapper');
 // initialize state
 
 // set event listeners to update state and DOM
 
-//I know this isn't right, but this is where we got. 
-MadLipz.addEventListener('click') {
-    console.log('hello');
-}
-
-
+madLipz.addEventListener('click', () => {
+    inputWrapper.textContent = ('Shall I compare ' + inputOne.value + ' to a ' + inputTwo.value + '? ' +
+        'Thou art more ' + inputThree.value + ' and more ' + inputFour.value + '. ' +
+        'Rough winds do ' + inputFive.value + ' the ' + inputSix.value + inputSeven.value +
+        ' of May. ' +
+        'So long as ' + inputEight.value + ' can ' + inputNine.value + ', or eyes can see. ' +
+        'So long lives' + inputTen.value + ', and this ' + inputEleven.value + ' life to thee.');
+});
